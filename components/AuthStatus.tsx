8a1d7ace2ad052818e490e33696d8eb73d906574
LighteningAB <fanpatrick8@gmail.com>
2025-09-16 16:40:25 +0100

    Update AuthStatus.tsx

--- components/AuthStatus.tsx
+++ components/AuthStatus.tsx
@@ -1,5 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, HStack, Text} from "@chakra-ui/react";
+
 
 export function AuthStatus() {
   const { data: session, status } = useSession();
@@ -7,10 +8,14 @@ export function AuthStatus() {
   if (!session)
     return <Button onClick={() => signIn()}>Sign in</Button>;
   return (
-    <Flex direction="row">
-      Signed in as {session.user?.email || session.user?.name}
-      <Button onClick={() => signOut()} ml = "0.5vw">Sign out</Button>
-    </Flex>
+    <HStack spacing={3} align="center">
+      <Text fontSize="sm" color="gray.600" maxW="20vw" isTruncated>
+        Signed in as {session.user?.email || session.user?.name}
+      </Text>
+      <Button onClick={() => signOut()}>
+        Sign out
+      </Button>
+    </HStack>
   );
 }
 
