57e0b3e447f8c37262d52cf1f0f418caa22bcc9d
Patrick Fan <124469718+lighteningAB@users.noreply.github.com>
2025-09-22 19:57:25 +0100

    adding in ace editor to render text area nicely (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,19 @@
 
 import { useState } from "react";
 import { Keyboard } from "../components/Keyboard";
-import { Heading, Textarea, Flex, Button, Select } from "@chakra-ui/react";
+import { Heading, Flex, Button, Select } from "@chakra-ui/react";
 import safeEval from "safe-eval";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 import LZString from "lz-string";
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import presets from "./presets/presets";
 import AuthStatus from "../components/AuthStatus";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-textmate";
 
 function KeyboardLayoutViewer() {
   const [jsonInput, setJsonInput] = useState(Object.values(presets)[0] || "");
@@ -47,7 +51,7 @@ function KeyboardLayoutViewer() {
     }
   }, [searchParams]);
 
-   const handlePresetChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handlePresetChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const presetName = e.target.value;
     if (presetName && presets[presetName]) {
       setJsonInput(presets[presetName]);
@@ -55,38 +59,50 @@ function KeyboardLayoutViewer() {
   };
 
   return (
-      <div>
-        <Flex justifyContent = "space-between" direction="row" w = "80%" alignItems="center" mx="auto" paddingTop="5">
-          <Heading>Keyboard Layout Viewer</Heading>
-          <Flex direction="row" alignItems="center">
-            <AuthStatus></AuthStatus>
-            <Button onClick={handleGoToPractice} ml="0.5vw">Go To Practice</Button>
-          </Flex>
+    <div>
+      <Flex
+        justifyContent="space-between"
+        direction="row"
+        w="80%"
+        alignItems="center"
+        mx="auto"
+        paddingTop="5"
+      >
+        <Heading>Keyboard Layout Viewer</Heading>
+        <Flex direction="row" alignItems="center">
+          <AuthStatus></AuthStatus>
+          <Button onClick={handleGoToPractice} ml="0.5vw">
+            Go To Practice
+          </Button>
         </Flex>
-        <Flex w = "80%" mx = "auto" paddingTop = "1vw" paddingBottom="1vw">
-          <Select width = "10%" onChange={handlePresetChange}>
-              <option selected hidden disabled value="">Presets</option>
-            {Object.keys(presets).map((label) =>(
-              <option key = {label} value = {label}>
-                {label}
-              </option>
-            ))}
-          </Select>
-        </Flex>
-        <Flex w = '100%' align="center" justify-content="center" direction="column">
-          <Textarea
-            value={jsonInput}
-            onChange={(e) => setJsonInput(e.target.value)}
-            rows={10}
-            cols={40}
-            width={'80%'}
-          />
-          <br />
-          <Button onClick={handleRender}>Render Keyboard</Button>
-          <br />
-          {layout.length > 0 && <Keyboard layout={layout} />}
-        </Flex>
-      </div>
+      </Flex>
+      <Flex w="80%" mx="auto" paddingTop="1vw" paddingBottom="1vw">
+        <Select width="10%" onChange={handlePresetChange}>
+          <option selected hidden disabled value="">
+            Presets
+          </option>
+          {Object.keys(presets).map((label) => (
+            <option key={label} value={label}>
+              {label}
+            </option>
+          ))}
+        </Select>
+      </Flex>
+      <Flex w="100%" align="center" justify-content="center" direction="column">
+        <AceEditor
+          mode="json"
+          theme="textmate"
+          value={jsonInput}
+          onChange={setJsonInput}
+          width="80%"
+          height="15vh"
+        />
+        <br />
+        <Button onClick={handleRender}>Render Keyboard</Button>
+        <br />
+        {layout.length > 0 && <Keyboard layout={layout} />}
+      </Flex>
+    </div>
   );
 }
 
